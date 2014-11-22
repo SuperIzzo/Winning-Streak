7547a712316ceca546926491dc27a4481526a534
Jake Thorne <jake2.thorne@live.uwe.ac.uk>
2014-11-22 00:50:52 +0000

    day 1 final

--- Assets/Scripts/Items/ItemStats.cs
+++ Assets/Scripts/Items/ItemStats.cs
@@ -4,4 +4,10 @@ using System.Collections;
 public class ItemStats : MonoBehaviour {
 
 	public float weight = 1;
+
+	void Update()
+	{
+		if(this.rigidbody.velocity.x < 0.01f && this.rigidbody.velocity.y < 0.01f && this.rigidbody.velocity.z < 0.01f)
+			this.rigidbody.Sleep();
+	}
 }
