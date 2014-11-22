9f26f5d1819469dea5c22b8bbf1300d7a099af05
Hristoz Stefanov <Hristoz2004@gmail.com>
2014-11-22 17:48:40 +0000

    Added AudioMan, it will create all the sound FX for the scene

---
+++ Assets/Sound/AudioMan.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AudioMan : MonoBehaviour
+{
+	public MyAudioSource audioSourceType;
+
+	public AudioClip ambience;
+	public List<AudioClip> soloDetailSounds;
+	public List<AudioClip> massDetailSounds;
+
+	public float hype;
+	public float hypeDecay = 0.01f;
+	public float minHype = 0.2f;
+	public float maxHype = 1.0f;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		audio.clip = ambience;
+		audio.Play();
+		//ambience.
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// Hype stuff, the crowd hype dies gradually
+		hype *= hypeDecay;
+ 		hype = Mathf.Clamp( hype, minHype, maxHype );
+		audio.volume = hype;
+
+		float hypeChance = hype * 0.1f; // 10% is a lot
+
+		if( Random.value < hypeChance )
+		{
+			if( Random.value > 0.6f )
+			{
+				float pan = 0.3f - Random.value * 0.6f;
+				float vol = Mathf.Min(1, Random.value * hype + 0.3f); 
+
+				AudioClip clip = massDetailSounds[ Random.Range(0, massDetailSounds.Count) ];
+
+				CreateSound( clip, pan, vol );
+			}
+			else
+			{
+				float pan = 0.5f - Random.value * 1f;
+				float vol = Mathf.Min(1, Random.value * hype + 0.3f); 
+				
+				AudioClip clip = soloDetailSounds[ Random.Range(0, massDetailSounds.Count) ];
+				
+				CreateSound( clip, pan, vol );
+			}
+		}
+	
+	}
+
+
+	void CreateSound( AudioClip clip, float relPan, float relVolume )
+	{
+		MyAudioSource source = (MyAudioSource) Instantiate( audioSourceType );
+
+		source.SetAudio( clip, relPan, relVolume );
+	}
+}
---
+++ Assets/Sound/MyAudioSource.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyAudioSource : MonoBehaviour
+{
+	public AudioClip audioClip;
+	float pan;
+	float vol;
+
+
+	public void SetAudio( AudioClip clip, float relPan, float relVol )
+	{
+		audioClip = clip; 
+		pan = relPan;
+		vol = relVol;
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		if( audioClip )
+		{
+			audio.volume = vol;
+			audio.pan = pan;
+			audio.clip = audioClip;
+			audio.Play();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if( audioClip==null || audio==null || !audio.isPlaying )
+		{
+			Destroy( gameObject );
+		}
+	}
+}
