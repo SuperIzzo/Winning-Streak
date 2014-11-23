7d994b4cb418cbc106eca9556b2432390e7ef766
Hristoz Stefanov <Hristoz2004@gmail.com>
2014-11-23 13:20:33 +0000

    The commentator system so far...

---
+++ Assets/Sound/Commentator.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public enum CommentatorEvent
+{
+	NONE = 0,
+	RANDOM,
+	PICKED_BALL,
+	DODGE_TACKLE
+}
+
+
+[System.Serializable]
+public class CommentatorQueue
+{
+	public string name;
+	public CommentatorEvent commentEvent;
+	public List<AudioClip> clipQueue;
+}
+
+
+public class Commentator : MonoBehaviour
+{
+	// Public / data
+	public List<CommentatorQueue> commentQueues;
+
+	// Internal / state
+	static Commentator instance;
+	CommentatorQueue currentQueue;
+	int clipIndex = 0;
+
+
+	// Returns a single instance for this component
+	public static Commentator GetInstance()
+	{
+		if( !instance )
+		{
+			instance = GameObject.FindObjectOfType<Commentator>();
+		}
+
+		return instance;
+	}
+
+
+	// Public interface
+	public bool Comment( CommentatorEvent evt )
+	{
+		// Pick a random comment which matches the event
+		int start = Random.Range(0, commentQueues.Count );
+		int i = start+1;
+
+		// repeat until we've cycled them all
+		while( i!=start )
+		{
+			// Wrap around if we have to
+			i = i % commentQueues.Count;
+
+			CommentatorQueue commentQueue = commentQueues[i];
+			if( commentQueue !=null
+			    && commentQueue.commentEvent == evt 
+			    && commentQueue.clipQueue !=null
+			    && commentQueue.clipQueue.Count>0 )
+			{
+				return PlayeQueue( commentQueue );
+			}
+
+			// to the next comment
+			i++;
+		}
+
+		return false;
+	}
+
+
+	// Sets the current queue
+	private bool PlayeQueue( CommentatorQueue queue )
+	{
+		// TODO: check event priority
+		clipIndex = 0;
+		currentQueue = queue;
+
+		return true;
+	}
+
+
+	// Updates the Commentator
+	void Update()
+	{
+		// If teh audio is not playing move onto the
+		// the next line in the script (if any left)
+		if( !audio.isPlaying )
+		{
+			if( currentQueue != null && clipIndex < currentQueue.clipQueue.Count-1 )
+			{
+				clipIndex++;
+				audio.clip = currentQueue.clipQueue[ clipIndex ];
+				audio.Play();
+			}
+		}
+	}
+}
---
+++ Assets/Sound/RandomCommentsGenerator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomCommentsGenerator : MonoBehaviour
+{
+	public float minSilence = 10.0f;
+	public float maxSilence = 20.0f;
+
+	public float commentCountDown = 0;
+
+	Commentator commentator;
+
+	// Use this for initialization
+	void Start ()
+	{
+		commentator = Commentator.GetInstance();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		commentCountDown -= Time.unscaledTime;
+
+		if( commentCountDown < 0 )
+		{
+			commentCountDown = Random.Range(minSilence, maxSilence);
+			commentator.Comment( CommentatorEvent.RANDOM );
+		}
+	}
+}
