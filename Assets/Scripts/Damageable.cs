80de8429ec818ec8fda69f0c5dc028389b0c0d30
Hristoz Stefanov <Hristoz2004@gmail.com>
2014-11-23 09:51:31 +0000

    Damageable to turn the damagee into a ragdoll, damager to turn a tell the damagable about the damage, and a throwable to enable a damager

---
+++ Assets/Scripts/Damageable.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Damageable : MonoBehaviour
+{
+	public GoRagdoll ragdoll;
+
+	public void Damage( Damager damager )
+	{
+		// Turn into ragdoll here
+		if( ragdoll )
+		{
+			ragdoll.KillPlayer();
+		}
+	}
+}
---
+++ Assets/Scripts/Throwable.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Throwable : MonoBehaviour
+{
+	public Damager damager;
+	int framesTillDisabled = 0;
+
+
+	void Start()
+	{
+		if( !damager )
+			damager = GetComponent<Damager>();
+
+		if( !damager )
+		{
+			damager = gameObject.AddComponent<Damager>();
+		}
+
+		damager.enabled = false;
+	}
+
+	// Use this for initialization
+	void OnCollisionEnter()
+	{
+		framesTillDisabled = 2;
+	}
+
+
+	void Throw()
+	{
+		damager.enabled = true;
+	}
+
+	void Update()
+	{
+		if( framesTillDisabled>0 )
+		{
+			framesTillDisabled -= 1;
+
+			if( framesTillDisabled<=0 )
+			{
+				damager.enabled = false;
+			}
+		}
+	}
+}
