d9c57601a4c04a9a1c6c8d105b294e04a3779d51
Hristoz Stefanov <Hristoz2004@gmail.com>
2014-11-28 13:28:09 +0000

    Added a simple character animator... this will grow later on (I pressume)

---
+++ Assets/Characters/_Common/Scripts/CharacterAnimator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//--------------------------------------------------------------
+/// <summary> Character animator. </summary>
+/// <description> 
+/// Animates the character 3D model based on the character 
+/// controller.
+/// </description>
+//--------------------------------------
+[AddComponentMenu("Character/Character Animator")]
+public class CharacterAnimator : MonoBehaviour
+{
+	[Range(0.0f,1.0f)]
+	public float goofiness;
+	public BaseCharacterController characterController;
+	public Animator animator;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		float 	speed		= characterController.relativeVelocity.magnitude;
+		bool  	dancing		= characterController.isDancing;
+		bool	tackling	= characterController.isTackling;
+
+		animator.SetFloat(	"speed",		speed		);
+		animator.SetFloat(	"goofiness",	goofiness	);
+		animator.SetBool(	"wiggle", 		dancing		);
+		animator.SetBool(	"tackle",		tackling	);
+	}
+}
