6c8eb6145383b8ffece2c6e6a10e7948f97c6cf6
Hristoz Stefanov <Hristoz2004@gmail.com>
2014-12-03 18:33:39 +0000

    The score panel buttons now work!

---
+++ Assets/Menus and GUI/GUIActions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIActions : MonoBehaviour
+{
+	public void RestartLevel()
+	{
+		Application.LoadLevel( Application.loadedLevel );
+	}
+
+	public void ChangeLevel( string levelName )
+	{
+		Application.LoadLevel( levelName );
+	}
+}
