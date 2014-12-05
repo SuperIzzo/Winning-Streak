a6598f566d5b2655c783aa1e7d0f945f4e9734dc
Hristoz Stefanov <Hristoz2004@gmail.com>
2014-12-05 23:47:36 +0000

    Some preliminary changes - factions and basic AI

---
+++ Assets/Characters/_Common/Scripts/Faction.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum FactionID
+{
+	STREAKER,
+	TEAM_A,
+	TEAM_B
+}
+
+public class Faction : MonoBehaviour
+{
+	public FactionID faction;
+
+
+	public bool IsAlly( FactionID otherFaction )
+	{
+		return this.faction == otherFaction;
+	}
+
+	public bool IsAlly( Faction faction )
+	{
+		if( faction )
+			return IsAlly( faction.faction );
+		else
+			return false;
+	}
+
+	public bool IsEnemy( FactionID otherFaction )
+	{
+		return !IsAlly(otherFaction);
+	}
+	
+	public bool IsEnemy( Faction faction )
+	{
+		if( faction )
+			return IsEnemy( faction.faction );
+		else
+			return true;
+	}
+}
