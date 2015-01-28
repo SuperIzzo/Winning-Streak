95b14b1593a204c9419df5949ecf2e5faa6bd778
Hristoz Stefanov <Hristoz2004@gmail.com>
2015-01-28 10:49:59 +0000

    Moved HitNoise to Props/_Common

---
+++ Assets/Props/_Common/HitNoise.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitNoise : MonoBehaviour
+{
+	public AudioClip hitSound;
+	public float collsionPowerRate = 5;
+
+	float soundTimer = 5.0f;
+
+	void Update()
+	{
+		if( soundTimer>0 )
+			soundTimer -= Time.deltaTime;
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if( audio && hitSound && soundTimer<=0 )
+		{
+			float collisionPower = collision.relativeVelocity.magnitude;
+			if( collisionPower > 2 )
+			{
+				audio.clip = hitSound;
+				audio.volume = Mathf.Clamp01( collisionPower/collsionPowerRate );
+				audio.Play();
+			}
+		}
+	}
+}
