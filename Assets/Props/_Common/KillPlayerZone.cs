18636d4a15d1382a68642625f931319c7eb59155
Hristoz Stefanov <Hristoz2004@gmail.com>
2015-01-28 11:00:17 +0000

    Moved KillPlayerZone and reworked it so that it activates on triggers as well

---
+++ Assets/Props/_Common/KillPlayerZone.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//--------------------------------------------------------------
+/// <summary> A zone that kills characters. </summary>
+//--------------------------------------
+public class KillPlayerZone : MonoBehaviour
+{
+	void OnCollisionEnter(Collision col)
+	{
+		KillPlayer( col.gameObject );
+	}
+
+	void OnTriggerEnter( Collider col )
+	{
+		KillPlayer( col.gameObject );
+	}
+
+	void KillPlayer( GameObject obj )
+	{
+		BaseCharacterController controller = obj.GetComponentInParent<BaseCharacterController>();
+		
+		if( controller )
+			controller.KnockDown();
+	}
+}
