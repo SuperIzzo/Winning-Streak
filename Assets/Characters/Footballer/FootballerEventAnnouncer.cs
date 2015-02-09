85f836a25698fafcff2cc713e87b530836a079ea
Hristoz Stefanov <Hristoz2004@gmail.com>
2015-02-09 08:02:01 +0000

    FootballerEventAnnouncer - to announce missed tackles.

---
+++ Assets/Characters/Footballer/FootballerEventAnnouncer.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootballerEventAnnouncer : MonoBehaviour
+{
+	public BaseCharacterController controller;
+	public float dodgeScore = 0.3f;
+
+	[Range(0,1)]
+	public float announcementChance = 0.3f;
+
+	public	float tackleMissTime = 5.0f;
+
+	private float tackleMissTimer;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if( controller.isTackling && tackleMissTimer<=0 )
+		{
+			tackleMissTimer = tackleMissTime;
+		}
+
+		if( tackleMissTimer>0 )
+		{
+			tackleMissTimer-= Time.deltaTime;
+
+			if( tackleMissTimer<=0 )
+			{
+				ResolveTackle();
+			}
+		}
+	}
+
+	void ResolveTackle()
+	{
+		BaseCharacterController player = Player.characterController;
+		ScoreManager score = GameSystem.score;
+		Commentator commentator = GameSystem.commentator;
+
+		if( !player.isKnockedDown )
+		{
+			score.AddMultPoint( 0.3f );
+
+			if( announcementChance > Random.value )
+			{
+				commentator.Comment( CommentatorEvent.DODGE_TACKLE );
+			}
+		}
+	}
+}
