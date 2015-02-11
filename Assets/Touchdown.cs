6665ca9a17312851e1d8d92682283768a0df8792
Hristoz Stefanov <Hristoz2004@gmail.com>
2015-02-11 12:54:58 +0000

    Touchdown event detector script added.

---
+++ Assets/Touchdown.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Touchdown : MonoBehaviour
+{
+	private static FactionID playerEndZone;
+
+	private Faction endZoneFaction;
+
+	// Use this for initialization
+	void Start()
+	{
+		playerEndZone = FactionID.STREAKER;
+
+		endZoneFaction = GetComponent<Faction>(); 
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		// A bunch of rules and conditions the collider has to satisfy: 
+		// * firstly it must be a ball
+		// * it must be owned by the player and the player must be
+		//	 holding it while passing (be in full possesion)
+		// * the player must be coming from the opposite end zone
+		var ball = other.GetComponent<ThrowableObject>();
+		if( ball!=null && ball.CompareTag( Tags.ball ) )
+		{
+			BaseCharacterController owner = ball.owner;
+			if( !ball.isThrown && owner.CompareTag( Tags.player ) )
+			{
+				if( endZoneFaction.IsEnemy(playerEndZone) )
+				{
+					TouchDown();
+				}
+			}
+		}
+	}
+
+	void TouchDown()
+	{
+		playerEndZone = endZoneFaction.faction;
+		GameSystem.scoringEvent.Fire( ScoringEvent.TOUCH_DOWN );
+	}
+}
