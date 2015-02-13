902ad19f1a77a04dddbe7584e135052ffc5d7cd6
Hristoz Stefanov <Hristoz2004@gmail.com>
2015-02-13 10:25:30 +0000

    Simplified FollowTarget.cs

--- Assets/System/Camera/FollowTarget.cs
+++ Assets/System/Camera/FollowTarget.cs
@@ -13,40 +13,32 @@ public class FollowCameraConfig
 
 public class FollowTarget : MonoBehaviour
 {
-
-	private Transform target;
-
 	public FollowCameraConfig normal;
 	public FollowCameraConfig zoomed;
 
+	private Transform target;
 	private FollowCameraConfig currentConfig;
 
 	void Start () 
 	{
 		currentConfig = normal;
+		target = Player.transform;
 	}
 
 	void Update () 
 	{
-        if (target)
-        {
-            Quaternion targetRot = Quaternion.LookRotation(target.position - this.transform.position);
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetRot, Time.unscaledDeltaTime * currentConfig.turnSpeed);
+	    Quaternion targetRot = Quaternion.LookRotation(target.position - this.transform.position);
+	    this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetRot, Time.unscaledDeltaTime * currentConfig.turnSpeed);
 
-            //side to side tracking
-            Vector3 newPos = new Vector3(target.position.x, currentConfig.positionOffset.y, currentConfig.positionOffset.z);
+	    //side to side tracking
+	    Vector3 newPos = new Vector3(target.position.x, currentConfig.positionOffset.y, currentConfig.positionOffset.z);
 			this.transform.position = Vector3.Lerp(this.transform.position, newPos, Time.unscaledDeltaTime * currentConfig.followSpeed);
 
-            //fov tracking
-            Vector3 invertedCamera = new Vector3(this.transform.position.x, this.transform.position.y,
-                                                 -this.transform.position.z - 10);
+	    //fov tracking
+	    Vector3 invertedCamera = new Vector3(this.transform.position.x, this.transform.position.y,
+	                                         -this.transform.position.z - 10);
 
-            camera.fieldOfView = Vector3.Distance(target.position, invertedCamera) * currentConfig.FOVModifier;
-        }
-        else
-        {
-			target = Player.transform;
-        }
+	    camera.fieldOfView = Vector3.Distance(target.position, invertedCamera) * currentConfig.FOVModifier;
 	}
 
 	public void ZoomIn()
