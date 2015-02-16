ef74c6187ba903b2394e3cbc5b14696890b20544
Hristoz Stefanov <Hristoz2004@gmail.com>
2015-02-16 19:29:21 +0000

    Footballer difficulty ups the playerHate and footballer speed when difficulty increases

---
+++ Assets/Characters/Footballer/FootballerDifficulty.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootballerDifficulty : MonoBehaviour
+{
+	private AIInput aiInput;
+	private BaseCharacterController controller;
+
+	private float baseMovementSpeed = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		aiInput = GetComponent<AIInput>();
+		controller = GetComponent<BaseCharacterController>();
+
+		if( controller )
+			baseMovementSpeed = controller.movementSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float difficultyLevel = GameSystem.difficulty.level;
+
+		if( aiInput )
+			aiInput.playerHate = difficultyLevel;
+
+		if( controller )
+			controller.movementSpeed = baseMovementSpeed + difficultyLevel*2;
+	
+	}
+}
