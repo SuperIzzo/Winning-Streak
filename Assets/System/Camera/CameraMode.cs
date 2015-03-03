bbe44f16253454adcc0fe5253edda6d6aefc98e9
Hristoz Stefanov <Hristoz2004@gmail.com>
2015-03-03 19:31:50 +0000

    A simple camera mode switcher

---
+++ Assets/System/Camera/CameraMode.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMode : MonoBehaviour
+{
+	public MonoBehaviour[] modes;
+
+	private int currentMode = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+		UpdateModes();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if( Input.GetButtonDown( "CameraMode" ) )
+		{
+			NextMode();
+		}
+	}
+
+	void NextMode()
+	{
+		do
+		{
+			currentMode++;
+			currentMode %= modes.Length;
+		}
+		while( modes[currentMode]== null );
+
+		UpdateModes();
+	}
+
+	void UpdateModes()
+	{
+		foreach( MonoBehaviour mode in modes )
+		{
+			mode.enabled = false;
+		}
+
+		modes[currentMode].enabled = true;
+	}
+}
