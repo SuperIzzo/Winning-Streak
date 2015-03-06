2557385fbd12f728091d6a7780a71e6f2e5c1ff1
Hristoz Stefanov <Hristoz2004@gmail.com>
2015-03-06 13:04:00 +0000

    Goals cannot be score twice on the same goal post unless you wait

--- Assets/Environment/Scripts/GoalScore.cs
+++ Assets/Environment/Scripts/GoalScore.cs
@@ -3,12 +3,38 @@ using System.Collections;
 
 public class GoalScore : MonoBehaviour
 {
+	private static GoalScore lastGoalScore = null;
+	private static float goalLockTime = 10.0f;
+	private static float goalLockTimer = 0.0f;
+
+
+	void Update()
+	{
+		// After 10 (or so) seconds unlock the goal post
+		if( goalLockTimer>0 )
+		{
+			goalLockTimer -= Time.deltaTime;
+			if( goalLockTimer<=0 )
+			{
+				lastGoalScore = null;
+			}
+		}
+	}
+
 	void OnTriggerEnter(Collider other)
 	{
 		if( other.CompareTag(Tags.ball) )
 		{
-			ScoringEventManager scoringEvent = GameSystem.scoringEvent;
-			scoringEvent.Fire( ScoringEvent.SCORED_GOAL );
+			// You cannot score two goals in a row
+			// in the same goal post (you have to alternate)
+			if( lastGoalScore!=this )
+			{
+				ScoringEventManager scoringEvent = GameSystem.scoringEvent;
+				scoringEvent.Fire( ScoringEvent.SCORED_GOAL );
+
+				lastGoalScore = this;
+				goalLockTimer = goalLockTime;
+			}
 		}
 	}
 
