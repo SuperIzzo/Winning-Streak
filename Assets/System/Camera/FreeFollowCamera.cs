3e32b9cd60f988b8186bdbfec4a395100713efe6
Hristoz Stefanov <Hristoz2004@gmail.com>
2015-03-08 11:29:04 +0000

    Added Freefollow camera that stays at a fixed distance from the target and has full freedom of movement (unlike the slide)

---
+++ Assets/System/Camera/FreeFollowCamera.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FreeFollowCamera : MonoBehaviour
+{
+	public FollowCameraConfig config;
+
+	private Transform target;
+
+	void Start ()
+	{
+		target = Player.transform;
+	}
+
+	void OnEnable()
+	{
+		if( !target )
+			target = Player.transform;
+
+		config.positionOffset = transform.position - target.position;
+
+		Vector3 targetDir = transform.forward * config.positionOffset.magnitude;
+		config.lookAtOffset = (transform.position + targetDir) - target.position;
+	}
+
+	// Use this for initialization
+	void Update ()
+	{
+		Quaternion targetRot = Quaternion.LookRotation((target.position + config.lookAtOffset) - this.transform.position);
+		this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetRot, Time.unscaledDeltaTime * config.turnSpeed);
+		
+		//side to side tracking
+		Vector3 newPos = target.position + config.positionOffset;
+		this.transform.position = Vector3.Lerp(this.transform.position, newPos, Time.unscaledDeltaTime * config.followSpeed);
+	}
+}
