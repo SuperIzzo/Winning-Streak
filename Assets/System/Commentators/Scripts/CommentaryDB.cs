5d72a26350ba6a2d49a5ca415190b74fe0756979
Hristoz Stefanov <Hristoz2004@gmail.com>
2015-03-26 21:28:29 +0000

    ADDED: CommentaryDB script (was supposed to be bundled with the commit before the last one)

---
+++ Assets/System/Commentators/Scripts/CommentaryDB.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class CommentaryDB : ScriptableObject
+{
+	//--------------------------------------------------------------
+	/// <summary> A commentator conversation queue. </summary>
+	//-------------------------------------- 
+	[System.Serializable]
+	public class CommentatorQueue
+	{
+		public string name;
+		public CommentatorEvent commentEvent;
+		public List<AudioClip> clipQueue;
+	}
+
+	#region Public settings
+	public List<CommentatorQueue> commentQueues;
+	#endregion
+}
