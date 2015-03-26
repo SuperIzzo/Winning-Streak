7a72f455ce006762c0685394076bb8e2cc4fe59a
Hristoz Stefanov <Hristoz2004@gmail.com>
2015-03-26 21:21:13 +0000

    ADDED: CommentaryDB scriptable object and asset This is the same list that used to be in the Commentator prefab

---
+++ Assets/Editor/Assets/CommentaryDBAsset.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class CommentaryDBAsset
+{
+	[MenuItem("Assets/Create/Database/Commentary")]
+	public static void CreateAsset ()
+	{
+		ScriptableObjectUtility.CreateAsset<CommentaryDB> ();
+	}
+}
