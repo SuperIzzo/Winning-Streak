e3f1b0ea69f8cc4026835c5cb6995a4f084915f9
Hristoz Stefanov <Hristoz2004@gmail.com>
2015-04-04 00:24:42 +0100

    REACTOR: Changed ScoreManager with Score and a few other components

---
+++ Assets/System/Score/HypeScore.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//--------------------------------------------------------------
+/// <summary> Hype score. </summary>
+//--------------------------------------
+[RequireComponent(typeof(Score))]
+public class HypeScore : MonoBehaviour
+{
+	//--------------------------------------------------------------
+	/// <summary> Reference to score </summary>
+	//--------------------------------------
+	private Score score;
+
+	//--------------------------------------------------------------
+	/// <summary> Initialises this instance. </summary>
+	//--------------------------------------
+	void Start ()
+	{
+		score = GetComponent<Score>();
+	}
+	
+	//--------------------------------------------------------------
+	/// <summary> Updates this instance. </summary>
+	//--------------------------------------
+	void Update ()
+	{
+		score.baseScore +=  Time.deltaTime * GameSystem.crowd.hype;
+	}
+}
