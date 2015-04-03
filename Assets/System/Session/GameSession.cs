c88d188afccb662f210c6cb2f7c89346fd7254ad
Hristoz Stefanov <Hristoz2004@gmail.com>
2015-04-04 00:31:31 +0100

    ADDED: GameSession which only tracks time atm

---
+++ Assets/System/Session/GameSession.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSession : MonoBehaviour
+{
+	public static GameSession current
+	{
+		get
+		{
+			if( !_current )
+			{
+				_current = GameObject.FindObjectOfType<GameSession>();
+			}
+			
+			return _current;
+		}
+	}
+	private static GameSession _current;
+
+
+	public float timePlayed {get; private set;}
+
+	// Use this for initialization
+	void Start ()
+	{
+		timePlayed = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		timePlayed += Time.deltaTime;
+	}
+}
