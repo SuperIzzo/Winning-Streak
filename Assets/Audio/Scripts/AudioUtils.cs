a0502e1c439430dd9cf707d1fdf5ebc9dd97d3d6
Hristoz Stefanov <Hristoz2004@gmail.com>
2015-04-08 15:37:55 +0100

    REFACTOR: MyAudioSource replaced by two classes: * "DestroyOnAudioCompletion" to destroy a game object that no longer play clips * AudioUtils to factory create the sound effects

---
+++ Assets/Audio/Scripts/AudioUtils.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+//--------------------------------------------------------------
+/// <summary> Audio utilities. </summary>
+//-------------------------------------- 
+public static class AudioUtils
+{
+	//--------------------------------------------------------------
+	/// <summary> Component list for oneshot sounds. </summary>
+	//--------------------------------------
+	private static readonly System.Type[] ONE_SHOT_SOUND_COMPONENTS =
+	{
+		typeof(AudioSource),
+		typeof(DestroyOnAudioSourceCompletion)
+	};
+
+	//--------------------------------------------------------------
+	/// <summary> Plays one shot sound effect. </summary>
+	/// <returns> The constructed game object.</returns>
+	/// <param name="parent">Parent</param>
+	/// <param name="clip">Clip</param>
+	/// <param name="pan">Pan</param>
+	/// <param name="vol">Volume</param>
+	//--------------------------------------
+	public static GameObject PlayOneShot( Component parent, AudioClip clip, float pan, float vol )
+	{
+		AudioMixerGroup parentMixerGroup = null;
+		GameObject go = new GameObject( "SoundEffect", ONE_SHOT_SOUND_COMPONENTS );
+		
+		if( parent )
+		{
+			// Group all created objects under the parent
+			go.transform.parent = parent.transform;
+
+			AudioSource parentSource = parent.GetComponent<AudioSource>();
+			if( parentSource )
+			{
+				parentMixerGroup = parentSource.outputAudioMixerGroup;
+			}
+		}
+		
+		AudioSource audioSource = go.GetComponent<AudioSource>();
+		audioSource.volume = vol;
+		audioSource.panStereo = pan;
+		audioSource.clip = clip;
+		audioSource.outputAudioMixerGroup = parentMixerGroup;
+
+		audioSource.Play();
+
+		return go;
+	}
+}
---
+++ Assets/Audio/Scripts/DestroyOnAudioSourceCompletion.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//-------------------------------------------------------------
+/// <summary> An utility class that has the responsibility of
+/// 	      destroying the gameObject when the audio clip
+///	      stops playing </summary>
+//--------------------------------------
+public class DestroyOnAudioSourceCompletion : MonoBehaviour
+{
+	AudioSource audio;
+
+	void Start()
+	{
+		audio = GetComponent<AudioSource>();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if( audio==null || !audio.isPlaying )
+		{
+			Destroy( gameObject );
+		}
+	}
+}
