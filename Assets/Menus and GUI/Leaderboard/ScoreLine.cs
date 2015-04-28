710b5eb7cc64535900ff90c226040115a3db442e
Hristoz Stefanov <Hristoz2004@gmail.com>
2015-04-28 19:25:18 +0100

    ADDED: a simple accessor for the text objects in a score Line

---
+++ Assets/Menus and GUI/Leaderboard/ScoreLine.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreLine : MonoBehaviour
+{
+	//--------------------------------------------------------------
+	#region Inspector settings
+	//--------------------------------------
+	[SerializeField] Text _rankText;
+	[SerializeField] Text _nameText;
+	[SerializeField] Text _scoreText;
+	[SerializeField] Text _timeText;
+	#endregion
+
+
+	//--------------------------------------------------------------
+	#region Public properties
+	//--------------------------------------
+	public string rank
+	{
+		get{ return _rankText.text;  }
+		set{ _rankText.text = value; }
+	}
+
+	public string name
+	{
+		get{ return _nameText.text;  }
+		set{ _nameText.text = value; }
+	}
+
+	public string score
+	{
+		get{ return _scoreText.text;  }
+		set{ _scoreText.text = value; }
+	}
+
+	public string time
+	{
+		get{ return _timeText.text;  }
+		set{ _timeText.text = value; }
+	}
+	#endregion
+}
