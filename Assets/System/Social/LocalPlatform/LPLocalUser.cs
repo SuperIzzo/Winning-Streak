4b96a11c506a55295f2ebae5b8d78d688b25fe8f
Hristoz Stefanov <Hristoz2004@gmail.com>
2015-05-14 18:07:17 +0100

    CHANGED: a few minor things: simplified properties, namespace

--- Assets/System/Social/LocalPlatform/LPLocalUser.cs
+++ Assets/System/Social/LocalPlatform/LPLocalUser.cs
@@ -1,32 +1,36 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.SocialPlatforms;
 
-namespace LocalPlatform
+namespace RoaringSnail.SocialPlatforms.LocalImpl
 {
-	class LocalUser : UserProfile, ILocalUser
-	{
-		private bool		_authenticated = false;
-		private bool		_underage = false;
-		
-		
-		// Local user has no friends
-		public IUserProfile[] 	friends{ get { return new IUserProfile[]{}; } }
-		public bool 			authenticated{ get{ return _authenticated; } }
-		public bool 			underage{ get{ return _underage; } }
-		
-		public void Authenticate( System.Action<bool> callback )
-		{
-			_authenticated = true;
-			_underage = false;
-			
-			callback( true );
-		}
-		
-		public void LoadFriends( System.Action<bool> callback )
-		{
-			throw new System.NotImplementedException();
-			
-			callback( true );
-		}
-	}
+    class LocalUser : UserProfile, ILocalUser
+    {
+        // Local user has no friends
+        public IUserProfile[] friends { get { return new IUserProfile[] { }; } }
+        public bool authenticated { get; protected set; }
+        public bool underage { get; protected set; }
+
+
+        public LocalUser(string userID)
+        {
+            authenticated = false;
+            underage = false;
+            id = userID;
+        }
+
+        public void Authenticate(System.Action<bool> callback)
+        {
+            authenticated = true;
+            underage = false;
+
+            callback(true);
+        }
+
+        public void LoadFriends(System.Action<bool> callback)
+        {
+            throw new System.NotImplementedException();
+
+            callback(true);
+        }
+    }
 }
