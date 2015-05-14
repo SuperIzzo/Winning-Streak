e00abeaae12cb6071529b6ff4c9ecdf1556cffef
Hristoz Stefanov <Hristoz2004@gmail.com>
2015-05-14 14:30:53 +0100

    ADDED: LeaderboardID - named constant strings for the leaderboard ids

---
+++ Assets/Utility/LeaderboardID.cs
@@ -0,0 +1,10 @@
+﻿//--------------------------------------------------------------
+/// <summary>Leaderboard id constants</summary>
+//--------------------------------------
+static class LeaderboardID
+{
+    public static readonly string TOTAL_SCORE = "lv01_total_score";
+    public static readonly string BASE_SCORE = "lv01_base_score";
+    public static readonly string MULT_SCORE = "lv01_mult_score";
+    public static readonly string TIME = "lv01_time";
+}
