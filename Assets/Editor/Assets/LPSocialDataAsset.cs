812085e208a5122bba554bee9b9f3d1fd52d10c0
Hristoz Stefanov <Hristoz2004@gmail.com>
2015-05-14 15:47:35 +0100

    ADDED: Social configuration scriptable object and asset

---
+++ Assets/Editor/Assets/LPSocialDataAsset.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+using RoaringSnail.SocialPlatforms.LocalImpl;
+
+public class LPSocialDataAsset
+{
+	[MenuItem("Assets/Create/Database/Local Social Data")]
+	public static void CreateAsset ()
+	{
+		ScriptableObjectUtility.CreateAsset<SocialConfiguration> ();
+	}
+}
