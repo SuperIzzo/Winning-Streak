90dd5e6c24e7c637e1258f6138c30910a2e5f867
Hristoz Stefanov <Hristoz2004@gmail.com>
2015-05-18 00:14:48 +0100

    REFACTORED: and reformatted FootballerDifficulty

--- Assets/Actors/Footballer/FootballerDifficulty.cs
+++ Assets/Actors/Footballer/FootballerDifficulty.cs
@@ -13,41 +13,75 @@
  * <date>    16-Feb-2015                                              </date> * 
 |*                                                                            *|
 \** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- **/
-using UnityEngine;
-using System.Collections;
-
-
-public class FootballerDifficulty : MonoBehaviour
+namespace RoaringSnail
 {
-	private AIInput aiInput;
-	private BaseCharacterController controller;
+    using UnityEngine;
 
-	private float baseMovementSpeed = 0.0f;
-	private float basePlayerHate	= 0.0f;
 
-	// Use this for initialization
-	void Start ()
-	{
-		aiInput = GetComponent<AIInput>();
-		controller = GetComponent<BaseCharacterController>();
 
-		if( controller )
-			baseMovementSpeed = controller.movementSpeed;
+    [AddComponentMenu("Winning Streak/Character/Footballer Difficulty", 101)]
+    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    /// <summary> Controls the footballers' stats based on difficulty.
+    ///                                                    </summary>
+    /// <remarks>
+    ///     Footballer Difficulty increases the difficulty of 
+    ///     footballers in respect with the global game difficulty.
+    /// </remarks>
+    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    public class FootballerDifficulty : MonoBehaviour
+    {
+        //..............................................................
+        #region             //  PRIVATE FIELDS  //
+        //--------------------------------------------------------------
+        private AIInput                     _aiInput;
+        private BaseCharacterController     _controller;
+        private float                       _baseMovementSpeed = 0.0f;
+        private float                       _basePlayerHate = 0.0f;
+        #endregion
+        //......................................
 
-		if( aiInput )
-			basePlayerHate = aiInput.playerHate;
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		float difficultyLevel = GameSystem.difficulty.level;
 
-		if( aiInput )
-			aiInput.playerHate = basePlayerHate + (1-basePlayerHate)*difficultyLevel;
+        //..............................................................
+        #region              //  PROTECTED METHODS  //
+        //--------------------------------------------------------------
+        /// <summary> Initializes the component </summary>
+        //--------------------------------------
+        protected void Start()
+        {
+            _aiInput = GetComponent<AIInput>();
+            _controller = GetComponent<BaseCharacterController>();
 
-		if( controller )
-			controller.movementSpeed = baseMovementSpeed + difficultyLevel*2;
-	
-	}
+            if (_controller)
+                _baseMovementSpeed = _controller.movementSpeed;
+
+            if (_aiInput)
+                _basePlayerHate = _aiInput.playerHate;
+        }
+
+
+
+        //--------------------------------------------------------------
+        /// <summary> Updates footballer stats by difficulty </summary>
+        //--------------------------------------
+        protected void Update()
+        {
+            float difficultyLevel = GameSystem.difficulty.level;
+
+            if (_aiInput)
+            {
+                _aiInput.playerHate = 
+                    _basePlayerHate  +  (1 - _basePlayerHate) * difficultyLevel;
+            }
+
+            if (_controller)
+            {
+                const int DIFFICULTY_MULT = 2;
+                _controller.movementSpeed =
+                    _baseMovementSpeed  +  difficultyLevel * DIFFICULTY_MULT;
+            }
+
+        }   
+        #endregion
+        //......................................
+    }
 }
