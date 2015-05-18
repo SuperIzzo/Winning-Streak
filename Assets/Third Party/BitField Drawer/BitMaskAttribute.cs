88ddab3ad650215fc1c0cd4a7da912b91ea40160
Hristoz Stefanov <Hristoz2004@gmail.com>
2015-05-18 14:36:53 +0100

    ADDED: a bitmask inspector drawer (like layers)

---
+++ Assets/Third Party/BitField Drawer/BitMaskAttribute.cs
@@ -0,0 +1,26 @@
+/** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- **\
+|*                                                                            *|
+ * <file>                  EnumBitMaskPropertyDrawer.cs               </file> * 
+ *                                                                            * 
+ * <copyright>                                                                * 
+ *   Copyright (C) 2013  Bunny83                                              * 
+ *                                                                            * 
+ *   cc-wiki, sharealine, attribution                                         * 
+ *   http://creativecommons.org/licenses/by-sa/2.5/                           *
+ *                                                                            *
+ *   original work:                                                           *
+ *   http://answers.unity3d.com/questions/393992/                             *
+ *           custom-inspector-multi-select-enum-dropdown.html                 *
+ *                                                               </copyright> * 
+ *                                                                            * 
+ * <author>  Bunny83                                                </author> * 
+ * <date>    05-Feb-2013                                              </date> * 
+|*                                                                            *|
+\** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- **/
+using UnityEngine;
+
+
+// Have to be defined somewhere in a runtime script file
+public class BitMaskAttribute : PropertyAttribute
+{
+}
