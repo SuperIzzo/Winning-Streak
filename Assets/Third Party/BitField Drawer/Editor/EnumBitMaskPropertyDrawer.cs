6e1c8d558e28740f69acfb2558de88c763718dfb
Hristoz Stefanov <Hristoz2004@gmail.com>
2015-05-19 18:46:02 +0100

    FIXED: unused local variable

--- Assets/Third Party/BitField Drawer/Editor/EnumBitMaskPropertyDrawer.cs
+++ Assets/Third Party/BitField Drawer/Editor/EnumBitMaskPropertyDrawer.cs
@@ -72,7 +72,6 @@ public class EnumBitMaskPropertyDrawer : PropertyDrawer
 {
     public override void OnGUI(Rect position, SerializedProperty prop, GUIContent label)
     {
-        var typeAttr = attribute as BitMaskAttribute;
         // Add the actual int value behind the field name
         label.text = label.text + "(" + prop.intValue + ")";
         prop.intValue = EditorExtension.DrawBitMaskField(position, prop.intValue, fieldInfo.FieldType, label);
