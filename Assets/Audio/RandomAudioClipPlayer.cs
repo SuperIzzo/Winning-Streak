16c50fce05311ef08431810b253d0f4de420d570
Hristoz Stefanov <Hristoz2004@gmail.com>
2015-05-19 08:35:58 +0100

    CHANGED: RandomCharacterShouts is now the more generic RandomAudioClipPlayer
    Which does the same, except it does not consider whether the character is dead or alive
    that has to be controlled externally via enabling/disabling the component

---
+++ Assets/Audio/RandomAudioClipPlayer.cs
@@ -0,0 +1,76 @@
+/** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- **\
+|*                                                                            *|
+ * <file>                   RandomCharacterShouts.cs                  </file> * 
+ *                                                                            * 
+ * <copyright>                                                                * 
+ *   Copyright (C) 2015  Roaring Snail Limited - All Rights Reserved          * 
+ *                                                                            * 
+ *   Unauthorized copying of this file, via any medium is strictly prohibited * 
+ *   Proprietary and confidential                                             * 
+ *                                                               </copyright> * 
+ *                                                                            * 
+ * <author>  Hristoz Stefanov                                       </author> * 
+ * <date>    14-Feb-2015                                              </date> * 
+|*                                                                            *|
+\** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- **/
+namespace RoaringSnail
+{
+    using UnityEngine;
+    using System.Collections;
+    
+
+
+    [RequireComponent(typeof(AudioSource))]
+    [AddComponentMenu("Audio/Random Audio Player", 101)]
+    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    /// <summary> Plays random audio clips </summary>
+    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    public class RandomAudioClipPlayer : MonoBehaviour
+    {
+        [SerializeField] AudioClip[]    _audioClips = null;
+        [SerializeField] MinMax         _timeSpan;
+
+        private AudioSource             _audio;
+
+
+        //--------------------------------------------------------------
+        /// <summary> Initializes the audio player </summary>
+        //--------------------------------------
+        protected void Start()
+        {
+            StartCoroutine(PlayAudio());
+        }
+
+
+
+        //--------------------------------------------------------------
+        /// <summary> Refreshes external references </summary>
+        //--------------------------------------
+        protected void OnEnable()
+        {
+            _audio = GetComponent<AudioSource>();
+        }
+
+
+
+        //--------------------------------------------------------------
+        /// <summary> Plays random audio clips at random intervals </summary>
+        //--------------------------------------
+        IEnumerator PlayAudio()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(_timeSpan.Random());
+
+                // TODO: Play sounds only of the character's not dead
+                if( enabled && _audio &&
+                    _audioClips!=null && 
+                    _audioClips.Length>0 )
+                {
+                    _audio.clip = _audioClips[Random.Range(0, _audioClips.Length)];
+                    _audio.Play();
+                }
+            }
+        }
+    }
+}
