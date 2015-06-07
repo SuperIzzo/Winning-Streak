a37386f988978a3dd3b1f04a5d303f975e34ec77
Hristoz Stefanov <Hristoz2004@gmail.com>
2015-06-07 11:01:34 +0100

    ADDED: a static class to hold control names

---
+++ Assets/Framework/Utility/InputControls.cs
@@ -0,0 +1,35 @@
+/** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- **\
+|*                                                                            *|
+ * <file>                      InputControls.cs                       </file> * 
+ *                                                                            * 
+ * <copyright>                                                                * 
+ *   Copyright (C) 2015  Roaring Snail Limited - All Rights Reserved          * 
+ *                                                                            * 
+ *   Unauthorized copying of this file, via any medium is strictly prohibited * 
+ *   Proprietary and confidential                                             * 
+ *                                                               </copyright> * 
+ *                                                                            * 
+ * <author>  Hristoz Stefanov                                       </author> * 
+ * <date>    07-Jun-2015                                              </date> * 
+|*                                                                            *|
+\** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- **/
+namespace RoaringSnail.WinningStreak
+{
+    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    /// <summary> Holds input control names. </summary>
+    /// <remarks>
+    ///     Should be used instead of litterals in code.
+    /// </remarks>
+    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    static class InputControls
+    {
+        public static readonly string horizontal    = "Horizontal";
+        public static readonly string vertical      = "Vertical";
+        public static readonly string wiggle        = "Wiggle";
+        public static readonly string dash          = "Dash";
+        public static readonly string grab          = "Grab";
+        public static readonly string sloMo         = "SlowMo";
+        public static readonly string pause         = "Pause";
+        public static readonly string screenshot    = "Screenshot";
+    }
+}
