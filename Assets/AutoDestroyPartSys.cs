cedea3b8e2119b9feea079fe7d033e05a1d2ce26
Izzo <hristoz2004@gmail.com>
2015-10-07 23:30:40 +0100

    Unity5 no longer has the option to autodestroy particles... so I added one

---
+++ Assets/AutoDestroyPartSys.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class AutoDestroyPartSys : MonoBehaviour
+{
+    private ParticleSystem ps;
+
+
+    public void Awake()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    public void Update()
+    {
+        if( ps )
+        {
+            if( !ps.IsAlive() )
+            {
+                Destroy( gameObject );
+            }
+        }
+    }
+}
