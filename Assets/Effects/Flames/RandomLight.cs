837faedd80352e6a8a2335ad0b600115d2297293
Izzo <hristoz2004@gmail.com>
2015-10-07 23:31:32 +0100

    Added dust and flames effects (particles)

---
+++ Assets/Effects/Flames/RandomLight.cs
@@ -0,0 +1,48 @@
+﻿/** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- **\
+|*                                                                            *|
+ * <file>                        RandomLight.cs                       </file> * 
+ *                                                                            * 
+ * <copyright>                                                                * 
+ *   Copyright (C) 2015  Roaring Snail Limited - All Rights Reserved          * 
+ *                                                                            * 
+ *   Unauthorized copying of this file, via any medium is strictly prohibited * 
+ *   Proprietary and confidential                                             * 
+ *                                                               </copyright> * 
+ *                                                                            * 
+ * <author>  Hristoz Stefanov                                       </author> * 
+ * <date>    06-Oct-2015                                              </date> * 
+|*                                                                            *|
+\** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- **/
+namespace RoaringSnail.WinningStreak
+{
+    using UnityEngine;
+
+
+    public class RandomLight : MonoBehaviour
+    {
+        [SerializeField]
+        float _intensityMin;
+
+        [SerializeField]
+        float _intensityMax;
+
+        [SerializeField]
+        float _intensitySmoothness;
+
+        Light _light;
+
+        protected void Awake()
+        {
+            _light = GetComponent<Light>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float newIntensity = Random.Range( _intensityMin, _intensityMax );
+            _light.intensity = 
+                _light.intensity * _intensitySmoothness
+                + newIntensity * (1 - _intensitySmoothness);
+        }
+    }
+}
