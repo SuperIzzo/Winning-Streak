3f2941fce02e80e98ee94644472da1eb0cd29edf
Izzo <hristoz2004@gmail.com>
2015-10-07 23:28:00 +0100

    Billboard simplified again

--- Assets/Framework/Utility/Billboard.cs
+++ Assets/Framework/Utility/Billboard.cs
@@ -19,33 +19,12 @@ namespace RoaringSnail.WinningStreak
 
 
 
-    [System.Flags]
-    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    /// <summary> Rotates the transform towards a target </summary>
-    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    internal enum AxesFlags
-    {
-        X = (1 << 0),
-        Y = (1 << 1),
-        Z = (1 << 2),
-    }
-
-
-
     [AddComponentMenu("Miscellaneous/Billboard", 102)]
     //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     /// <summary> Rotates the transform towards a target </summary>
     //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     public class Billboard : MonoBehaviour
     {
-        //--------------------------------------------------------------
-        /// <summary> A bitmask containing all three axis flags </summary>
-        //--------------------------------------
-        static readonly AxesFlags allAxes 
-                                    = AxesFlags.X | AxesFlags.Y | AxesFlags.Z;
-
-
-
         //..............................................................
         #region            //  INSPECTOR FIELDS  //
         //--------------------------------------------------------------
@@ -55,13 +34,6 @@ namespace RoaringSnail.WinningStreak
         [Tooltip("The object this billboard rotates towards. \n" +
                  "Defaults to the main camera.")]
         [SerializeField]            Transform _lookAt           = null;
-
-
-        //--------------------------------------------------------------
-        /// <summary> Freeze rotation along axes </summary>
-        //--------------------------------------
-        [Tooltip("Freeze rotation along axes")]
-        [SerializeField, BitMask]   AxesFlags _freezeRotation   = 0;
         #endregion
         //......................................
 
@@ -87,62 +59,11 @@ namespace RoaringSnail.WinningStreak
         {
             if( _lookAt )
             {
-                // If there's no freezing we just look at target
-                if ( _freezeRotation == 0)
-                {
-                    transform.LookAt(_lookAt);
-                }
-                // else if not all axes are frozen
-                // we rotate partially
-                else if( !AreAllAxesFrozen() )
-                {
-                    Vector3 oldRotation = transform.rotation.eulerAngles;
-
-                    transform.LookAt(_lookAt);
-
-                    Vector3 newRotation = transform.rotation.eulerAngles;
-
-                    // Eliminate individual axes
-                    newRotation = FreezeIndividualAxes( _freezeRotation, 
-                                                        oldRotation, 
-                                                        newRotation  );
-                    
-
-                    transform.rotation = Quaternion.Euler(newRotation);
-                }
+                transform.LookAt( _lookAt );
             }
         }
 
 
-
-        //--------------------------------------------------------------
-        /// <summary> Sellectively assigns axes from old to new </summary>
-        //--------------------------------------
-        private Vector3 FreezeIndividualAxes(   AxesFlags axes, 
-                                                Vector3 oldRotation, 
-                                                Vector3 newRotation )
-        {
-            if ((axes & AxesFlags.X) > 0)
-                newRotation.x = oldRotation.x;
-
-            if ((axes & AxesFlags.Y) > 0)
-                newRotation.y = oldRotation.y;
-
-            if ((axes & AxesFlags.Z) > 0)
-                newRotation.z = oldRotation.z;
-
-            return newRotation;
-        }
-
-
-
-        //--------------------------------------------------------------
-        /// <summary> Returns whether rotation is frozen on all axes </summary>
-        //--------------------------------------
-        private bool AreAllAxesFrozen()
-        {
-            return (_freezeRotation & allAxes) == allAxes;
-        }
         #endregion
         //......................................
     }
