6709e1976ddad20a62f2031dba30b8d7b5fad9c4
Izzo <hristoz2004@gmail.com>
2015-10-07 23:37:57 +0100

    Updated the fireworks machine

---
+++ Assets/Props/Fireworks Machine/FireworksActivator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireworksActivator : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _fireWorks;
+
+	protected void OnCollisionEnter(Collision col)
+    {
+        if( Random.value < 0.3f )
+        {
+            _fireWorks.SetActive( !_fireWorks.activeSelf );
+        }
+    }
+}
